36c5ab4bbea8c57e600644f51375e4010c98ed0d
barna7en <barna7en@gmail.com>
2025-07-11 22:57:45 +0500

    Предварительный образец_перераспределение

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class Student:
         all_grades = [grade for grades_list in self.grades.values() for grade in grades_list]
         return round(mean(all_grades), 1) if all_grades else None
 
+    # переопределяем магические методы для студентов
     def __str__(self):
         avg_grade = self.average_grade()
         finished_courses_str = ', '.join(self.finished_courses)
@@ -55,6 +56,7 @@ class Lecturer(Mentor):
         all_grades = [grade for grades_list in self.grades.values() for grade in grades_list]
         return round(mean(all_grades), 1) if all_grades else None
 
+    # переопределяем магические методы для Лекторов
     def __str__(self):
         avg_grade = self.average_grade()
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade}'
@@ -84,14 +86,18 @@ class Reviewer(Mentor):
 
 
 # Примеры использования
+# Здесь мы определим студента, лектора и ревьювера
 if __name__ == "__main__":
     best_student = Student('Руслан', 'Петухов', 'Мужской')
     some_lecturer = Lecturer('Сергей', 'Семёнович')
     some_reviewer = Reviewer('Александр', 'Александрович')
-
-    best_student.courses_in_progress.extend(['Python', 'Git'])
+# Наш "лучший" (определим его в начале) студент закончил курс введение в программирование
     best_student.finished_courses.append('Введение в программирование')
+# А так же курсы в процессе изучения "Python и Git"
+    best_student.courses_in_progress.extend(['Python', 'Git'])
+# Лектор ведет эти курсы
     some_lecturer.courses_attached.extend(['Python', 'Git'])
+# Ревьювер проверяющий задания по этим курсам
     some_reviewer.courses_attached.extend(['Python', 'Git'])
 
     # Студенческие оценки за ДЗ
@@ -103,19 +109,28 @@ if __name__ == "__main__":
     best_student.rate_lecture(some_lecturer, 'Python', 9)
     best_student.rate_lecture(some_lecturer, 'Git', 8)
 
-    print("\nСтудент:")
+    # Добавим второго студента
+    another_student = Student('Антон', 'Ковалёв', 'Мужской')
+
+    # Второй студент изучает те же курсы
+    another_student.courses_in_progress.extend(['Python', 'Git'])
+
+    # Его оценки за проделанные работы
+    some_reviewer.rate_hw(another_student, 'Python', 7)
+    some_reviewer.rate_hw(another_student, 'Python', 6)
+    some_reviewer.rate_hw(another_student, 'Git', 8)
+
+    # Сравнение студентов и лекторов
+    print("\nСтудент1:")
     print(best_student)
+    print("\nСтудент2:")
+    print(another_student)
 
     print("\nПреподаватель:")
     print(some_lecturer)
 
     print("\nЭксперт:")
     print(some_reviewer)
-
-    # Сравнение студентов и лекторов
-    another_student = Student('Антон', 'Ковалёв', 'Мужской')
-    another_student.courses_in_progress.append('Python')
-    some_reviewer.rate_hw(another_student, 'Python', 7)
 
     print(f'\nЛучший студент лучше другого студента? {"Да" if best_student > another_student else "Нет"}')
 
